9977cd5be4bfb3c7849b83e19c7e56b48104fd14
Jesse Miller <jesse.miller@adops.com>
2016-06-28 14:26:41 -0500

    forwarded not forwrded

--- charm.go
+++ charm.go
@@ -164,7 +164,7 @@ func (conf Config) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				r.URL.Host,
 				r.URL.Path,
 				r.URL.RawQuery,
-				r.Header["X-Forwrded-Email"],
+				r.Header["X-Forwarded-Email"],
 			)
 			// get the cached response
 			response, err := http.ReadResponse(
@@ -185,7 +185,7 @@ func (conf Config) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.URL.Host,
 		r.URL.Path,
 		r.URL.RawQuery,
-		r.Header["X-Forwrded-Email"],
+		r.Header["X-Forwarded-Email"],
 	)
 	upstreamURL, err := url.Parse(conf.Upstream)
 	if err != nil {
