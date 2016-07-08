69050c0f47985ecf3df4b554cdad305e3bff9b9e
Jesse Miller <jesse.miller@adops.com>
2016-07-08 11:28:23 -0500

    if all responses are errors, responds with one

--- transport.go
+++ transport.go
@@ -16,7 +16,11 @@ type stableTransport struct {
 // stableTransport.RoundTrip makes many round trips and returns the first
 // response
 func (t *stableTransport) RoundTrip(r *http.Request) (*http.Response, error) {
-	c := make(chan *http.Response)
+	// channel to send the fisrt good response
+	rc := make(chan *http.Response)
+	// channel to send and collect bad (error) responses
+	ec := make(chan *http.Response)
+	// use the default transport from http if not specified
 	if t.wrappedTransport == nil {
 		t.wrappedTransport = http.DefaultTransport
 	}
@@ -29,8 +33,13 @@ func (t *stableTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 				log.Printf("transport-error: %v", err)
 				return
 			}
+			// don't send anything if the response is not good
+			if resp.StatusCode != 200 {
+				ec <- resp
+				return
+			}
 			select {
-			case c <- resp:
+			case rc <- resp:
 				// they were still waiting for the first
 				// response and they received it from c
 				return
@@ -42,7 +51,24 @@ func (t *stableTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 		}()
 	}
 
-	// wait to receive the first response
-	first := <-c
-	return first, nil
+	// wait to receive the first good response but gather error responses
+	// start with a slice of responses to gather bad responses. If this has
+	// as many responses in it as requests we have made, we know there is no
+	// good response coming back so can respond with a summary of the bad
+	// responses.
+	errorResponses := make([]*http.Response, 0)
+	for {
+		select {
+		case firstGoodResponse := <-rc:
+			return firstGoodResponse, nil
+		case errorResponse := <- ec:
+			errorResponses = append(errorResponses, errorResponse)
+			// if the length of that slice grows to ReqFanFactor
+			if len(errorResponses) >= t.reqFanFactor {
+				// respond with a sample of the errors.
+				// TODO summarize the errors into a new response
+				return errorResponses[0], nil
+			}
+		}
+	}
 }
